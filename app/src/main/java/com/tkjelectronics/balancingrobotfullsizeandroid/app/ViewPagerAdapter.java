4955565db8b2c6767d8c0f95c90ea6f3a5f89198
Kristian Lauszus <lauszus@gmail.com>
2014-06-17 01:10:03 +0200

    Implemented Map tab, so the user can she his location on Google Maps

--- app/src/main/java/com/tkjelectronics/balancingrobotfullsizeandroid/app/ViewPagerAdapter.java
+++ app/src/main/java/com/tkjelectronics/balancingrobotfullsizeandroid/app/ViewPagerAdapter.java
@@ -19,17 +19,9 @@
 
 package com.tkjelectronics.balancingrobotfullsizeandroid.app;
 
-import android.content.Context;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.actionbarsherlock.app.SherlockFragment;
-
-import java.util.Locale;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     public static final int INFO_FRAGMENT = 0;
@@ -37,23 +29,19 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     public static final int MAP_FRAGMENT = 2;
     public static final int GRAPH_FRAGMENT = 3;
 
-    Context context;
-
-    public ViewPagerAdapter(Context context, FragmentManager fm) {
+    public ViewPagerAdapter(FragmentManager fm) {
         super(fm);
-        this.context = context;
     }
 
     @Override
-    public SherlockFragment getItem(int position) {
+    public Fragment getItem(int position) {
         switch (position) {
             case INFO_FRAGMENT:
                 return new InfoFragment();
             case PID_FRAGMENT:
                 return new PIDFragment();
             case MAP_FRAGMENT:
-                //return new MapFragment();
-                return PlaceholderFragment.newInstance(position + 1);
+                return new MapFragment();
             case GRAPH_FRAGMENT:
                 return new GraphFragment();
         }
@@ -67,7 +55,6 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public CharSequence getPageTitle(int position) {
-        Locale l = Locale.getDefault();
         switch (position) {
             case INFO_FRAGMENT:
                 return "Info";
@@ -79,36 +66,5 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
                 return "Graph";
         }
         return null;
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends SherlockFragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_balancing_robot_full_size, container, false);
-        }
     }
 }
